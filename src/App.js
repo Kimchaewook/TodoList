1d1024f75d3b673d0a2ae3785a3bef2b66852e69
Kimchaewook <allurings2@naver.com>
2023-01-04 17:50:40 +0900

    Test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
+  const Test = styled.div`
+    width: 200px;
+    height: 200px;
+    background-color: tomato;
+  `
+
   return (
-    <></>
+    <>
+      <Test/>
+    </>
   );
 }
 
